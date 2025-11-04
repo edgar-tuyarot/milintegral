37fa54e80b62bd0fc30e8d9e18ede57b252dd544
Edgar <edgar.tuyarot@gmail.com>
2025-11-04 02:24:04 -0300

    update de favicon

--- src/App.vue
+++ src/App.vue
@@ -2,3 +2,16 @@
 <template>
   <router-view/>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  metaInfo: {
+    title: 'Milintegral',
+    titleTemplate: ' Milintegral',
+    htmlAttrs: {
+      lang: 'es'
+    }
+  }
+}
+</script>
